92115896b7f5273251dc0db6e340a3f08c7ff5bf
Neeraj S D <neerajsd04@gmail.com>
2022-10-13 13:18:50 +0530

    intital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RequestHistory from './pages/request_history.component';
+
+function App() {
+  return (
+    <div className="App">
+      <RequestHistory />
+    </div>
+  );
+}
+
+export default App;
